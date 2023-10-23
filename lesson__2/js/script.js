b29be3a1b5362d6346373c45e8ad47c13f4e35ea
Anastasia <109382571+NastiaDuliba@users.noreply.github.com>
2023-10-23 12:32:14 +0200

    Delete lesson__2 directory

--- lesson__2/js/script.js
+++
@@ -1,7 +0,0 @@
-//let a =  parseFloat(prompt("введіть значення першого числа", "0"));
-//let b =  parseFloat(prompt("введіть значення другого числа", "0"));
-
-//let firstResult = a + b;
-//let twoResult = a / b;
-//console.log(firstResult)
-//console.log(twoResult);
